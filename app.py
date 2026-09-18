eafd4734bf6b423c2e715026b51c5cebe2638ea6
Annjohnson16 <148953632+Annjohnson16@users.noreply.github.com>
2026-09-18 09:33:03 +0530

    Modify AC units input range and prompt
    Updated input prompt and constraints for AC units.

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ poly = PolynomialFeatures(degree=2)
 st.title("Electric Bill Predictor")
 
 ac_units = st.number_input(
-    "Enter AC Units",
-    min_value=0.0,
+    "Enter AC Units(Between 1 nad 150)",
+    min_value=1.0,
+    max_value=149.0,
     step=1.0
 )
 
